e6c7d2bf0fe3ef8b7b69e83d92c4cd7ceb2823f5
Marco Torrente Corbett <marcotorrentecorbett@Marcos-MacBook-Air.local>
2025-05-08 18:53:36 +0200

    forth commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -53,4 +53,8 @@ export const Header = () => {
             
         }
     })
+
+    logo.addEventListener("click", async () => {
+        printImages();
+    })
 }
--- src/utils/fetchApi.js
+++ src/utils/fetchApi.js
@@ -4,7 +4,7 @@ const API_KEY = "vlgJ9oqFteWlvpQB4T5F8Au5VKvP3KYV4DXkmZ1X1Q0";
 
 const url = "https://api.unsplash.com/search/photos";
 
-export const fetchApi = async (query = "Gatos") => {
+export const fetchApi = async (query = "epic") => {
     try {
         const response = await fetch (`${url}/?query=${query}&per_page=20&client_id=${API_KEY}`);
         
